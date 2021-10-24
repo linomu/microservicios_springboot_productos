2b089913186eca11f2a4e780ae0ac74ba9e789cb
lino <linomu@unicauca.edu.co>
2021-10-23 23:14:52 -0500

    Seccion3: Eureka Server. Video 24

---
+++ src/main/java/com/formacionbdi/springboot/app/productos/models/entity/Producto.java
@@ -0,0 +1,77 @@
+package com.formacionbdi.springboot.app.productos.models.entity;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.persistence.Transient;
+
+@Entity
+@Table(name = "TBL_PRODUCTOS")
+public class Producto implements Serializable {
+
+	private static final long serialVersionUID = -1413945125229071483L;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(name = "NOMBRE")
+	private String nombre;
+	
+	@Column(name = "PRECIO")
+	private Double precio;
+
+	@Column(name = "CREATED_AT")
+	@Temporal(TemporalType.DATE)
+	private Date createdAt;
+	
+	@Transient
+	private Integer port;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public Double getPrecio() {
+		return precio;
+	}
+
+	public void setPrecio(Double precio) {
+		this.precio = precio;
+	}
+
+	public Date getCreatedAt() {
+		return createdAt;
+	}
+
+	public void setCreatedAt(Date createdAt) {
+		this.createdAt = createdAt;
+	}
+
+	public Integer getPort() {
+		return port;
+	}
+
+	public void setPort(Integer port) {
+		this.port = port;
+	}
+}
